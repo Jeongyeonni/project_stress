b288fe39024066a6193180218e96ea154c98eaec
Jeongyeonni <tlswjddus98@snu.ac.kr>
2022-10-14 18:17:47 +0900

    221014

--- smoking_gift.R
+++ smoking_gift.R
@@ -1,17 +1,58 @@
 library(dplyr)
+library(tidyverse)
+# setwd('/home/tlswjddus98/project_stress/project_stress')
 
-#read data
+# read data
+# Global variable
+FILE_NAME = 'stress_fMRI_smoking_221014.csv'
+# paste0(), strc 
+survey_smoking <- read.csv(FILE_NAME)
 
-survey_smoking <- read.csv("stress_fMRI_smoking_221014.csv")
 
-#SubjID, Session extract
-survey_smoking_ID <- survey_smoking %>% select(SubjID, Session)
-survey_smoking_ID <- survey_smoking_ID[-c(1:2),]
-write.csv(survey_smoking_ID, file = 'survey_smoking_ID', row.names = TRUE)
-#SubjID 별로 session 1,2,3 다 있는지 확인, 모두 있으면 1
-survey_smoking_com <- survey_smoking_ID %>% group_by(SubjID) %>% summarise(sum_session=n_distinct(Session))
-survey_smoking_com$complete <- ifelse(survey_smoking_com$sum_session >= 3, 1, 0)
-survey_smoking_com$sent <- 0
+# # SubjID, Session extract
+# survey_smoking_ID <- 
+#   survey_smoking %>% 
+#   select(SubjID, Session) %>% 
+#   slice(n():3) %>% 
+#   as_tibble() %>%
+#   view() %>%
+#   write.csv(survey_smoking_ID, file = 'survey_smoking_ID', row.names = TRUE)
+# 
+# # survey_smoking_ID <- survey_smoking_ID[-c(1:2),]
+# # write.csv(survey_smoking_ID, file = 'survey_smoking_ID', row.names = TRUE)
+# 
+# 
+# # SubjID 별로 session 1,2,3 다 있는지 확인, 모두 있으면 1
+# survey_smoking_com <- survey_smoking_ID %>% 
+#   group_by(SubjID) %>% 
+#   summarise(sum_session = n_distinct(Session)) %>%
+#   view() %>%
+#   write.csv(survey_smoking_com, file = 'survey_smoking_com.csv', row.names = TRUE)
+# 
+# 
+# survey_smoking %>% 
+#   select(SubjID, Session, StartDate, EndDate) %>% 
+#   slice(n():3) %>%
+#   as_tibble() %>%
+#   View()
 
-write.csv(survey_smoking_com, file = 'survey_smoking_com.csv', row.names = TRUE)
 
+
+###
+# Check with columns of interest
+
+# change order of columns
+survey_smoking %>% 
+  tail(-2, ) %>% 
+  relocate(c(SubjID, Session)) %>%
+  View() 
+
+
+survey_smoking %>% 
+  select(SubjID, Session) %>% 
+  slice(n():3) %>% 
+  as_tibble() %>% 
+  group_by(SubjID, Session) %>% 
+  tally()
+
+# survey_smoking %>% select(Finished)
