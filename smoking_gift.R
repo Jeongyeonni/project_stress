72fa4d899af1bfe86f4b4c28c99af5381bdb1b69
Jeongyeonni <tlswjddus98@snu.ac.kr>
2022-10-14 12:07:19 +0900

    221014 update

--- smoking_gift.R
+++ smoking_gift.R
@@ -11,6 +11,7 @@ View(survey_smoking_ID)
 #SubjID 별로 session 1,2,3 다 있는지 확인, 모두 있으면 1
 survey_smoking_com <- survey_smoking_ID %>% group_by(SubjID) %>% summarise(sum_session=n_distinct(Session))
 survey_smoking_com$complete <- ifelse(survey_smoking_com$sum_session >= 3, 1, 0)
+survey_smoking_com$sent <- 0
 
 write.csv(survey_smoking_com, file = 'survey_smoking_com.csv', row.names = TRUE)
 
