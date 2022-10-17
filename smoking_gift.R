7ef0ea8a3e51ff36705d9de1213c01066f05d42d
Jeongyeonni <tlswjddus98@snu.ac.kr>
2022-10-17 21:39:55 +0900

    221017 update

--- smoking_gift.R
+++ smoking_gift.R
@@ -5,10 +5,28 @@ library(tidyverse)
 # read data
 # Global variable
 FILE_NAME = 'stress_fMRI_smoking_221017.csv'
+TOTAL_SESSION = 4
+
 # paste0(), strc 
+
 survey_smoking <- read.csv(FILE_NAME)
 
 
+### check the completion of follow-up
+
+survey_com <- survey_smoking %>% 
+  select(SubjID, Session) %>% 
+  slice(n():3) %>% 
+  as_tibble() %>% 
+  group_by(SubjID) %>% 
+  summarise(Sum_session = n_distinct(Session)) %>%
+  mutate(Complete = ifelse(Sum_session == TOTAL_SESSION, 1, 0)) %>%
+  view()
+
+
+
+
+### ignore
 # # SubjID, Session extract
 # survey_smoking_ID <- 
 #   survey_smoking %>% 
@@ -35,22 +53,7 @@ survey_smoking <- read.csv(FILE_NAME)
 #   slice(n():3) %>%
 #   as_tibble() %>%
 #   View()
-
-
-
-
-# change order of columns
-survey_smoking %>% 
-  tail(-2, ) %>% 
-  relocate(c(SubjID, Session)) %>%
-  View() 
-
-
-survey_smoking %>% 
-  select(SubjID, Session) %>% 
-  slice(n():3) %>% 
-  as_tibble() %>% 
-  group_by(SubjID, Session) %>% 
-  tally() %>% view()
-
-# survey_smoking %>% select(Finished)
+#
+# # change order of columns
+# survey_smoking %>% 
+#   relocate(c(SubjID, Session)) 
